1ca3c09ea47aa045f891c7cc551ff9795e70021c
John C. Waters <john@johncwaters.com>
2023-06-30 15:29:52 -0600

    inital commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import image from "@astrojs/image";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://www.needalift.net/",
+  integrations: [tailwind(), sitemap(), image(), partytown()]
+});
